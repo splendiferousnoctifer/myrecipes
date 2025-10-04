9e6e20bee2fb999614df7257c9fc52446d3b50dd
Samuel Zühlke <63741955+splendiferousnoctifer@users.noreply.github.com>
2025-10-04 09:50:23 +0200

    Fix: Local development base path and GitHub Pages enablement

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/myrecipes/',
+  base: process.env.NODE_ENV === 'production' ? '/myrecipes/' : '/',
   server: {
     port: 3000
   }
